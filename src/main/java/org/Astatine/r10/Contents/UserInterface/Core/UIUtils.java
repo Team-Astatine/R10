0071598696660d3eb89724f620da3d3a81336fac
JwonLEE <jaxple@gmail.com>
2025-05-18 19:48:00 +0900

    Add: Essentials User 객체, performCommand() 함수 추가

--- src/main/java/org/Astatine/r10/Contents/UserInterface/Core/UIUtils.java
+++ src/main/java/org/Astatine/r10/Contents/UserInterface/Core/UIUtils.java
@@ -1,10 +1,13 @@
 package org.Astatine.r10.Contents.UserInterface.Core;
 
+import net.ess3.api.IEssentials;
+import net.ess3.api.IUser;
 import net.kyori.adventure.text.Component;
 import org.Astatine.r10.Enumeration.Type.ColorType;
 import org.Astatine.r10.Contents.UserInterface.Core.UIGenerator.CreatePanelItem;
 import org.Astatine.r10.Contents.UserInterface.Core.UIGenerator.SlotItemMapping;
 import org.Astatine.r10.Util.Function.StringComponentExchanger;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -18,6 +21,20 @@ import java.util.stream.Collectors;
 
 public abstract class UIUtils extends StringComponentExchanger {
     public final int MINIUM_TAB_CNT = 9;
+
+    private IEssentials getEssentialsAPI() {
+        return (IEssentials) Bukkit.getServer()
+                .getPluginManager()
+                .getPlugin("Essentials");
+    }
+
+    public IUser getEssentialPluginUserObject(Player player) {
+        return getEssentialsAPI().getUser(player.getUniqueId());
+    }
+
+    public void performCommand(Player player, String command, String targetName) {
+        player.performCommand(String.format("%s %s", command, targetName));
+    }
 
     public ItemStack getHeadItemStack(Player player) {
         ItemStack headItemStack = new ItemStack(Material.PLAYER_HEAD);
